5a4ba9978e3a6598407bdf4d170c2dae3b28942c
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2024-06-02 20:47:43 +0530

    some

--- server/src/config/dbConnection.ts
+++ server/src/config/dbConnection.ts
@@ -5,11 +5,11 @@ dotevn.config()
 // createing connnection to the mysql database
 const dbConnection = mysql.createConnection({
   connectionLimit: 12, // Adjust this value based on your requirements
-  host: process.env.HOST || 'localhost',  // Fallback to localhost if not specified
-  user: process.env.USER || 'root',
-  password: process.env.PASSWORD || 'admin',
-  database: process.env.NAME || 'labhkari',
-  port: process.env.DATABASE_PORT || 3306,
+  host: process.env.HOST ,  // Fallback to localhost if not specified
+  user: process.env.USER ,
+  password: process.env.PASSWORD,
+  database: process.env.NAME ,
+  port: process.env.DATABASE_PORT,
 
   // port: process.env.DATABASE_PORT
 } as PoolConfig);
